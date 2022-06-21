852b63475e80450b3c80382da4024a517dd88857
md198208 <md198208@gmail.com>
2022-06-20 22:24:37 -0500

    Add project files.

---
+++ OctoPawn/States/State.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OctoPawn.States
+{
+    public abstract class State
+    {
+        public Game1 game;
+
+        protected ContentManager content;
+
+        public State(Game1 game1, ContentManager content1)
+        {
+            game = game1;
+
+            content = content1;
+        }
+
+        public abstract void LoadContent();
+
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void PostUpdate(GameTime gameTime);
+
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+    }
+}
