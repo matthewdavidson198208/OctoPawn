562a1029a66a5eb62a1c063fcc8ee38db2f83e03
md198208 <md198208@gmail.com>
2022-06-22 23:10:18 -0500

    Added ability to drag and drop to move pawns

---
+++ OctoPawn/Components/Pawn.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Collisions;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using OctoPawn.States;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OctoPawn.Components
+{
+    public class Pawn : IEntity
+    {
+        public IShapeF Bounds { get; set; }
+        private AnimatedSprite _spriteSheet;
+        private Vector2 _spritePosition;
+        private SoundEffect _moveSound;
+        public bool IsDragged { get; set; }
+
+        private readonly GameState _gameState;
+        private readonly Game1 _game;
+
+        public ContentManager Content
+        {
+            get { return content; }
+        }
+        ContentManager content;
+
+        private MouseState _currentMouse;
+        private MouseState _previousMouse;
+        public EventHandler Click;
+        public bool Clicked { get; private set; }
+        public int ID { get; private set; }
+
+        public Pawn(IServiceProvider serviceProvider, Vector2 position, GameState gameState, bool isWhite, int id)
+        {
+            content = new ContentManager(serviceProvider, "Content");
+            _gameState = gameState;
+            _game = gameState.game;
+
+            Bounds = new RectangleF(position.X - 25, position.Y - 50, 50, 100);
+            ID = id;
+
+            SpriteSheet ss = null;
+            if(isWhite)
+                ss = Content.Load<SpriteSheet>("Sprites/white_pawn.sf", new JsonContentLoader());
+            else 
+                ss = Content.Load<SpriteSheet>("Sprites/black_pawn.sf", new JsonContentLoader());
+
+            _spriteSheet = new AnimatedSprite(ss);
+            _spriteSheet.Play("animation0");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _previousMouse = _currentMouse;
+            _currentMouse = Mouse.GetState();
+
+            var mouseRectangle = new Rectangle(_currentMouse.X, _currentMouse.Y, 1, 1);
+
+            var rect = new Rectangle((int)Bounds.Position.X, (int)Bounds.Position.Y, 50, 100);
+            if (mouseRectangle.Intersects(rect))
+            {
+                if (_currentMouse.LeftButton == ButtonState.Pressed)
+                {
+                    //_gameState.game.Select.Play(_gameState.game.SettingsManager.SFXManager.Volume,
+                    //    _gameState.game.SettingsManager.SFXManager.Pitch, _gameState.game.SettingsManager.SFXManager.Pan);
+                    Click?.Invoke(this, new EventArgs());
+                    IsDragged = true;
+                }
+                else
+                {
+                    IsDragged = false;
+                }
+            }
+
+            _spriteSheet.Play("animation0");
+            _spriteSheet.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //Currently for testing purposes
+            //spriteBatch.FillRectangle((RectangleF)Bounds, Color.Blue, 0.5f);
+
+            var position = new Vector2(Bounds.Position.X + 25, Bounds.Position.Y + 50);
+            spriteBatch.Draw(_spriteSheet, position, 0, new Vector2(0.4f, 0.4f));
+        }
+
+        public void OnCollision(CollisionEventArgs collisionInfo)
+        {
+            //throw new NotImplementedException();
+        }
+        public void CheckColor(Color color)
+        {
+            //throw new NotImplementedException();
+        }
+
+      
+    }
+}
--- OctoPawn/States/GameState.cs
+++ OctoPawn/States/GameState.cs
@@ -10,48 +10,68 @@ using MonoGame.Extended.Input;
 using MonoGame.Extended.Serialization;
 using MonoGame.Extended.Sprites;
 using OctoPawn.Components;
+using System.Linq;
+using Microsoft.Xna.Framework.Input;
 
 namespace OctoPawn.States
 {
     public class GameState : State
     {
         List<int> BoardState;
-        private Dictionary<int,AnimatedSprite> whitePawns { get; set; }
-        private Dictionary<int, AnimatedSprite> blackPawns { get; set; }
+        private List<Tuple<int,Pawn>> whitePawns { get; set; }
+        private List<Tuple<int, Pawn>> blackPawns { get; set; }
 
         public GameState(Game1 game1, ContentManager content1) : base(game1, content1)
         {
             BoardState = new List<int>() { 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 };
-            whitePawns = new Dictionary<int, AnimatedSprite>();
-            blackPawns = new Dictionary<int, AnimatedSprite>();
+            whitePawns = new List<Tuple<int, Pawn>>();
+            blackPawns = new List<Tuple<int, Pawn>>();
         }
 
         public override void LoadContent()
         {
             for(int i = 0; i < 4; i++)
             {
-                var whitePawn = new AnimatedSprite(content.Load<SpriteSheet>("Sprites/white_pawn.sf", new JsonContentLoader()));
-                whitePawn.Play("animation0");
-                whitePawns.Add((12 + i),whitePawn);
+                var whitePawn = new Pawn(game.Services, new Vector2((125 * i + 295), 570), this, true, i + 4)
+                {
+                    Click = new EventHandler(onHold),
+                };
+                whitePawns.Add(new Tuple<int, Pawn>((12 + i), whitePawn));
 
-                var blackPawn = new AnimatedSprite(content.Load<SpriteSheet>("Sprites/black_pawn.sf", new JsonContentLoader()));
-                blackPawn.Play("animation0");
-                blackPawns.Add(i, blackPawn);
+                var blackPawn = new Pawn(game.Services, new Vector2((125 * i + 295), 195), this, false, i)
+                {
+                    Click = new EventHandler(onHold),
+                };
+                blackPawns.Add(new Tuple<int, Pawn>(i, blackPawn));
             }
             
         }
 
+        private MouseState _currentMouse;
+        private int _pawnID;
+        private void onHold(object sender, EventArgs e)
+        {
+            var pawn = sender as Pawn;
+            if (pawn.ID != _pawnID && _pawnID != -1)
+                return;
+            _pawnID = pawn.ID;
+            _currentMouse = Mouse.GetState();
+            pawn.Bounds = new RectangleF(_currentMouse.Position.X - 25, _currentMouse.Position.Y - 50, 50, 100);
+        }
+
         public override void Update(GameTime gameTime)
         {
-            foreach(var white in whitePawns)
+            _currentMouse = Mouse.GetState();
+            if (_currentMouse.LeftButton == ButtonState.Released)
+                _pawnID = -1;
+
+            foreach (var white in whitePawns)
             {
-                white.Value.Play("animation0");
-                white.Value.Update(gameTime);
+                white.Item2.Update(gameTime);
             }
             foreach (var black in blackPawns)
             {
-                black.Value.Play("animation0");
-                black.Value.Update(gameTime);
+                black.Item2.Update(gameTime);
             }
         }
 
@@ -62,8 +82,6 @@ namespace OctoPawn.States
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
-            var scale = 0.4f;
-            //var scale = 0.8f;
             for (int i = 0; i < 4; i++)
             {
                 for(int j = 0; j < 4; j++)
@@ -78,19 +96,28 @@ namespace OctoPawn.States
                     }
                     spriteBatch.FillRectangle(new RectangleF(new Point2((125 * j + 245), (125 * i + 125)),
                                 new Size2(125, 125)), color);
+                }
+            }
 
-                    if(BoardState[(j + (4 * i))] == 1)
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    var pos = j + (4 * i);
+                    if (BoardState[pos] == 1)
                     {
-                        spriteBatch.Draw(whitePawns[(j + (4 * i))], new Vector2((125 * j + 295), (125 * i + 195)), 0, new Vector2(scale, scale));
+                        whitePawns.FirstOrDefault(x => x.Item1 == pos).Item2.Draw(spriteBatch);
+                        //spriteBatch.Draw(whitePawns[(j + (4 * i))], new Vector2((125 * j + 295), (125 * i + 195)), 0, new Vector2(scale, scale));
                     }
-                    else if(BoardState[(j + (4 * i))] == 2)
+                    else if (BoardState[pos] == 2)
                     {
-                        spriteBatch.Draw(blackPawns[(j + (4 * i))], new Vector2((125 * j + 295), (125 * i + 195)), 0, new Vector2(scale, scale));
+                        blackPawns.FirstOrDefault(x => x.Item1 == pos).Item2.Draw(spriteBatch);
+                        //spriteBatch.Draw(blackPawns[(j + (4 * i))], new Vector2((125 * j + 295), (125 * i + 195)), 0, new Vector2(scale, scale));
                     }
                 }
             }
 
-            spriteBatch.End();
+                    spriteBatch.End();
         }
 
     }
